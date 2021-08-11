cea8ed02ed8a77d845cc3c526e5d13fbbd44c2cf
Ken O'Brien <kenobrien@kens-mbp.lan>
2021-08-10 22:45:09 -0700

    updated imports

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import streamlit as st
 import webbrowser
 from datetime import date
 import yfinance as yf
-from prophet import Prophet
-from prophet.plot import plot_plotly
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 
